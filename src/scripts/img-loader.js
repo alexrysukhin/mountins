65097a9173e56811fbf1585b4876269684cc0778
Aleksandr Rysukhin <kruziatus@gmail.com>
2021-02-13 23:36:15 +0200

    better preloader

--- src/scripts/img-loader.js
+++ src/scripts/img-loader.js
@@ -1,54 +1,22 @@
-
+let images = Array.from(document.images);
+let imagesTotal = images.length;
+let imagesCout = 0;
 let preloader = document.querySelector('.preloader');
-let preloderPercents = document.querySelector('.preloader__percents');
-let array = Array.from(document.getElementsByTagName('img'));
-// let arrayAll = document.querySelectorAll('*');
-// let array = [];
+let preloaderDisplay = document.querySelector('.preloader__percents');
 
 
-// for(i = 0; i < arrayAll.length; i++) {
-//    if(arrayAll[i].hasAttribute('aria-hidden'))continue;   // <iframe>
-//    if(arrayAll[i].hasAttribute('src')){
-      
-//       array.push(arrayAll[i]);
-//       console.log(arrayAll[i])
-//    }
-// };
-// console.log(array);
-
-
-
-
-
-function percentsCount() {
-
-   let percentsTotal = 0;
-   let persents;
-   console.log(array);
-   function setPersents(current, total) {
-       persents = Math.floor(current / total * 100);
-   }
-   function loadImages() {
-      if(!array.length)  preloader.classList.add('preloader__not-visible');
-      for(i = 0; i < array.length;i++) {
-         array[i].addEventListener('load', function(){
-            percentsTotal++;
-            setPersents(percentsTotal, array.length);
-            preloderPercents.innerHTML = persents  + '%';
-            if( persents >= 100)preloader.classList.add('preloader__not-visible');
-            setTimeout(() =>preloader.classList.add('preloader__not-visible'),6000 )//если что-то идет не так с загрузкой
-         });
-      } 
-      
-   }
-   loadImages();
-   
-   
+for(let i = 0; i < imagesTotal; i++) {
+   image = new Image();
+   image.src = images[i].src;
+   image.addEventListener('load', loadImage);
+   image.addEventListener('error', loadImage);
 }
-percentsCount();
 
 
+function loadImage() {
+   imagesCout++;
+   let percents = Math.floor(imagesCout / imagesTotal * 100);
+   preloaderDisplay.innerHTML = percents + '%';
 
-
-
-
+   if(percents >= 100) preloader.classList.add('preloader__not-visible');
+};
